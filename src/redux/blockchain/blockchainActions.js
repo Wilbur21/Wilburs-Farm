28626b31aaec1f98faa72f9ec87f163f7aea2207
Wilbur21 <Wilbur1@mailfence.com>
2021-11-03 03:34:43 +0000

    Commit 2

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -51,10 +51,10 @@ export const connect = () => {
           method: "net_version",
         });
         // const NetworkData = await SmartContract.networks[networkId];
-        if (networkId == 1) {
+        if (networkId == 4) {
           const SmartContractObj = new Web3EthContract(
             SmartContract,
-            "0x688db0131c807a3495c23bc1b25726a76ea31f49"
+            "0xf80341A616b036F309D7de382448c23B3eA90f1d"
           );
           dispatch(
             connectSuccess({
